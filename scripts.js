a2c889233a9825ad9c7520587206f71a9cd75745
Ayana Hawk <ayanahawk11@gmail.com>
2019-08-23 18:14:39 -0400

    added pictures

--- scripts.js
+++ scripts.js
@@ -1,13 +1,28 @@
 const cards = document.querySelectorAll('.memory-card');
 
-function flipCard() {
-  // adding and removing another class "flip"
-  this.classList.toggle('flip');
-}
+// have to make has flipped constant
+let hasFlipped = false;
+let firstCard;
+let secondCard;
 
-/**
- * Event lsitener that loops and grabs each card and onClick,
- *invoked flipCard function
- */
+function flipCard() {
+  // giving the card a flip class
+  this.classList.add('flip');
+
+  // setting the cards to flip twice, fires only on the seccond click.
+  if (!hasFlipped) {
+    hasFlipped = true;
+    firstCard = this;
+  } else {
+    // seccond click
+    hasFlipped = false;
+    secondCard = this;
+    // do the cards matach?
+    console.log(firstCard, secondCard);
+    console.log(secondCard.dataset.framework);
+
+    // do the cards match?
+  }
+}
 
 cards.forEach(card => card.addEventListener('click', flipCard));
